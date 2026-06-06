eb4988bd8f3cd46e476edb9827a0873d61698abc
joberland <suniagajoberland@gmail.com>
2026-06-06 18:35:30 -0400

    copla

--- datos.js
+++ datos.js
@@ -3,8 +3,10 @@
 // =========================================================================
 export const CONFIG_MULTIMEDIA = {
   fallbacks: {
-    imagen: "https://images.unsplash.com/photo-1531403009284-440f080d1e12?w=400",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-smart-home-interface-screens-42352-large.mp4",
+    imagen:
+      "https://images.unsplash.com/photo-1531403009284-440f080d1e12?w=400",
+    video:
+      "https://assets.mixkit.co/videos/preview/mixkit-smart-home-interface-screens-42352-large.mp4",
   },
   ajustes: {
     lazyLoading: true,
@@ -23,23 +25,28 @@ export const INVENTARIO_LOCAL = [
     nombre: "Cinta LED RGBIC Inteligente 5M",
     categoria: "Luces LED",
     precio: 25.0,
-    imagen: "https://images.unsplash.com/photo-1565814636199-ae8133055c1c?auto=format&fit=crop&q=80&w=400",
-    video: "",
+    imagen:
+      "https://images.unsplash.com/photo-1565814636199-ae8133055c1c?auto=format&fit=crop&q=80&w=400",
+    video:
+      "https://static.videezy.com/system/resources/previews/000/012/979/original/record_player_011.mp4",
   },
   {
     id: "prod-002",
     nombre: "Switch Inteligente Domótica 3 Canales",
     categoria: "Domótica",
     precio: 18.5,
-    imagen: "https://images.unsplash.com/photo-1558002038-1055907df827?auto=format&fit=crop&q=80&w=400",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-smart-home-interface-screens-42352-large.mp4",
+    imagen:
+      "https://images.unsplash.com/photo-1558002038-1055907df827?auto=format&fit=crop&q=80&w=400",
+    video:
+      "https://static.videezy.com/system/resources/previews/000/041/735/original/75.Bitcoin.mp4",
   },
   {
     id: "prod-003",
     nombre: "Router TP-Link Archer AX55 Wi-Fi 6",
     categoria: "Conectividad",
     precio: 89.99,
-    imagen: "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?auto=format&fit=crop&q=80&w=400",
+    imagen:
+      "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?auto=format&fit=crop&q=80&w=400",
     video: "",
   },
   {
@@ -47,7 +54,17 @@ export const INVENTARIO_LOCAL = [
     nombre: "Bombillo Inteligente RGB+CCT Wi-Fi",
     categoria: "Luces LED",
     precio: 12.5,
-    imagen: "https://images.unsplash.com/photo-1550524514-9b6d630f9cc3?auto=format&fit=crop&q=80&w=400",
+    imagen:
+      "https://images.unsplash.com/photo-1550524514-9b6d630f9cc3?auto=format&fit=crop&q=80&w=400",
+    video: "",
+  },
+  {
+    id: "prod-005",
+    nombre: "Bombillo  RGB+CCT Wi-Fi",
+    categoria: "cualquier cosa",
+    precio: 12.5,
+    imagen:
+      "https://images.unsplash.com/photo-1550524514-9b6d630f9cc3?auto=format&fit=crop&q=80&w=400",
     video: "",
   },
 ];
